59c3e25dee3d0833069941ce77e935026812c6dd
Nekoyuki <hiyokommm@gmail.com>
2017-12-05 22:29:44 +0900

    test.html updated

--- restapi/restapi.py
+++ restapi/restapi.py
@@ -4,11 +4,13 @@
 
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import pandas as pd
 import io
 
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 db = SQLAlchemy(app)
 
