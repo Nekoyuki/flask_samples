73248e25355e338d61797782d43e921df99da966
Nekoyuki <hiyokommm@gmail.com>
2017-12-21 22:02:42 +0900

    test.py updated

--- restapi/test.py
+++ restapi/test.py
@@ -8,10 +8,16 @@ import restapi
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = restapi.app.test_client()
+        self.db = restapi.db.create_all()
 
-    def test_get(self):
-        res = self.app.get('/')
-        assert res.status_code == 404
+    def test_get_root(self):
+        r = self.app.get('/')
+        assert r.status_code == 404
+
+    def test_get_sqlalc(self):
+        r = self.app.get('/sqlalc/')
+        assert r.status_code == 200
+        assert b'"r": "GET success"' in r.data
 
 if __name__ == '__main__':
     unittest.main()
